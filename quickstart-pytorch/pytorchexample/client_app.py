d307b563182ca07d09f4eb9a5c3517c8856e8471
RishiKrishnah <rishikrishnah@gmail.com>
2026-06-04 22:21:55 +0530

    Added malicious client simulation

--- quickstart-pytorch/pytorchexample/client_app.py
+++ quickstart-pytorch/pytorchexample/client_app.py
@@ -6,19 +6,13 @@ from flwr.client import NumPyClient
 from .model import load_model
 
 from .task import (
-
     load_data,
-
     train,
-
     test,
-
     DEVICE
 )
 
-from .utils import (
-    poison_parameters
-)
+from .utils import poison_parameters
 
 
 class FlowerClient(NumPyClient):
@@ -27,19 +21,13 @@ class FlowerClient(NumPyClient):
 
         self.client_id = client_id
 
-        self.model = load_model().to(
-            DEVICE
-        )
+        self.model = load_model().to(DEVICE)
 
         self.trainloader, self.testloader = (
-
             load_data(client_id)
         )
 
-    def get_parameters(
-        self,
-        config
-    ):
+    def get_parameters(self, config):
 
         return [
 
@@ -49,71 +37,50 @@ class FlowerClient(NumPyClient):
             self.model.state_dict().items()
         ]
 
-    def set_parameters(
-        self,
-        parameters
-    ):
+    def set_parameters(self, parameters):
 
         params_dict = zip(
-
             self.model.state_dict().keys(),
-
             parameters
         )
 
         state_dict = {
 
-            k: torch.tensor(v)
+            k: torch.tensor(v).to(DEVICE)
 
             for k, v in params_dict
         }
 
         self.model.load_state_dict(
-
             state_dict,
-
             strict=True
         )
 
-    def fit(
-        self,
-        parameters,
-        config
-    ):
+    def fit(self, parameters, config):
 
         print(
-            f"\nClient "
-            f"{self.client_id} "
+            f"Client {self.client_id} "
             f"training..."
         )
 
-        self.set_parameters(
-            parameters
-        )
+        self.set_parameters(parameters)
 
         train(
-
             self.model,
-
             self.trainloader,
-
             epochs=1
         )
 
-        updated_params = (
-            self.get_parameters({})
-        )
+        updated_params = self.get_parameters({})
 
-        # Malicious client simulation
+        # Simulate malicious client
         if self.client_id == 2:
 
             print(
-                "\nMalicious "
-                "client detected!"
+                "Client 2 is malicious!"
             )
 
             updated_params = (
-
                 poison_parameters(
                     updated_params
                 )
@@ -123,9 +90,7 @@ class FlowerClient(NumPyClient):
 
             updated_params,
 
-            len(
-                self.trainloader.dataset
-            ),
+            len(self.trainloader.dataset),
 
             {
                 "client_id":
@@ -134,22 +99,15 @@ class FlowerClient(NumPyClient):
         )
 
     def evaluate(
-
         self,
-
         parameters,
-
         config
     ):
 
-        self.set_parameters(
-            parameters
-        )
+        self.set_parameters(parameters)
 
         loss, accuracy = test(
-
             self.model,
-
             self.testloader
         )
 
@@ -157,9 +115,7 @@ class FlowerClient(NumPyClient):
 
             float(loss),
 
-            len(
-                self.testloader.dataset
-            ),
+            len(self.testloader.dataset),
 
             {
                 "accuracy":
@@ -171,11 +127,7 @@ class FlowerClient(NumPyClient):
 def client_fn(context):
 
     client_id = (
-
-        context.node_config[
-            "partition-id"
-        ]
-
+        context.node_config["partition-id"]
         + 1
     )
 
@@ -185,6 +137,5 @@ def client_fn(context):
 
 
 app = fl.client.ClientApp(
-
     client_fn=client_fn
 )
