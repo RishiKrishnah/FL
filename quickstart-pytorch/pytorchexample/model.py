c94545e1d1b135263930224ae8019427b0a8508c
RishiKrishnah <rishikrishnah@gmail.com>
2026-07-25 09:07:03 +0530

    added resnet18 without pretraining

--- quickstart-pytorch/pytorchexample/model.py
+++ quickstart-pytorch/pytorchexample/model.py
@@ -693,7 +693,7 @@ def load_model(model_name):
     # ResNet18
     # ------------------------------------------------------
     elif model_name == "resnet18":
-        model = resnet18(weights="IMAGENET1K_V1")
+        model = resnet18(weights=None)
 
         model.fc = nn.Linear(
             model.fc.in_features,
